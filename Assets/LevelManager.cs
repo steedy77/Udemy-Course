331c2e46ed292e6745e9afc4fa227a52c88c120e
Hayden Steedman <hayden_steedman@hotmail.com>
2017-01-03 15:01:27 +1300

    Number Wizard
    UI Stuff

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void loadlevel(string name){
+		Application.LoadLevel(name);
+	}
+
+	public void QuitRequest () {
+	}
+}
